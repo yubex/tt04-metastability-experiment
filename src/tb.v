c8b38ff31a9c33dcb1eece96320a17ac53bd8597
yubex <118074126+yubex@users.noreply.github.com>
2023-09-07 17:39:22 +0200

    Add files via upload

---
+++ src/tb.v
@@ -0,0 +1,77 @@
+`default_nettype none
+`timescale 1us/1ns
+
+module tb (
+    );
+
+	reg clk;
+	reg rst_n;
+	reg trigger;
+	reg mode;
+	reg [5:0] delay_ctrl;
+	
+    wire [7:0] inputs = {delay_ctrl, mode, trigger};
+    wire [7:0] outputs;
+	
+	always 
+	begin
+		clk <= 1'b0;
+		# 10;
+		clk <= 1'b1;
+		# 10;
+	end 
+	
+	initial 
+	begin
+    $display("Simulation started!");
+		rst_n      <= 1'b0;
+		trigger    <= 1'b0;
+		delay_ctrl <= 6'b000001;
+		mode       <= 1'b1;
+		# 5000;
+		rst_n <= 1'b1;
+		# 50000;
+		trigger  <= 1'b1;
+		# 500000;
+		trigger  <= 1'b0;
+		# 500000;
+		trigger  <= 1'b1;
+		# 500000;
+		trigger  <= 1'b0;
+		# 500000;
+		trigger  <= 1'b1;
+		# 6100;
+        trigger  <= 1'b0;
+		# 50000;        
+		trigger  <= 1'b1;
+		# 500000; 
+		trigger  <= 1'b0;
+		# 6100;
+		trigger  <= 1'b1;
+		# 50000;
+		mode <= 1'b0;
+		# 500000;
+    $display("Done!");
+    $finish ;
+	end 
+
+    tt_yubex_metastability_experiment tt_yubex_metastability_experiment (
+         .ui_in(inputs),   // Dedicated inputs - connected to the input switches
+         .uo_out(outputs), // Dedicated outputs - connected to the 7 segment display
+         .uio_in(),        // IOs: Bidirectional Input path
+         .uio_out(),       // IOs: Bidirectional Output path
+         .uio_oe(),        // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+         .ena(1'b1),       // will go high when the design is enabled
+         .clk(clk),        // clock
+         .rst_n(rst_n)     // reset_n - low to reset        
+    );
+
+initial
+ begin
+    $dumpfile("test.vcd");
+    $dumpvars(0,clk,rst_n,trigger,mode,delay_ctrl,outputs);
+ end
+
+
+
+endmodule
---
+++ src/yubex_metastability_experiment.v
@@ -0,0 +1,137 @@
+
+/*
+metastability experiment
+*/
+
+`default_nettype none
+
+module tt_yubex_metastability_experiment (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+  
+    wire rst;
+    wire trigger;
+    wire mode;
+    wire [5:0] delay_ctrl;
+  
+    assign rst        = ! rst_n;
+    assign trigger    = ui_in[0];
+    assign mode       = ui_in[1];
+    assign delay_ctrl = ui_in[7:2];
+
+    localparam trigger_sr_size = 32;
+    reg [trigger_sr_size-1:0] trigger_sr;
+    
+    reg toggle_dff;
+    reg toggle_dff_en, toggle_dff_en_1t, toggle_dff_en_2t, toggle_dff_en_3t;
+    (* dont_touch = "yes" *) wire [128:0] prog_delay;
+    (* dont_touch = "yes" *) wire delayed_toggle_dff;
+
+    reg meta_dff_0, meta_dff_1;
+    reg meta_err_detected;
+
+
+  // shift trigger input into shift register
+  always @(posedge clk or posedge rst)
+  begin
+	if(rst) begin
+       		    trigger_sr <= {trigger_sr_size{1'b0}};
+    	    end 
+    else begin
+        trigger_sr <= {trigger_sr[trigger_sr_size-2:0],trigger};
+    end
+  end 
+
+  // analyze shift register values and generate data input for meta_dff
+  always @(posedge clk or posedge rst)
+  begin
+	if(rst) begin
+                toggle_dff       <= 1'b0;
+                toggle_dff_en    <= 1'b0;
+                toggle_dff_en_1t <= 1'b0;
+                toggle_dff_en_2t <= 1'b0;
+                toggle_dff_en_3t <= 1'b0;
+    	    end 
+    else begin
+        toggle_dff_en    <= 1'b0;
+        toggle_dff_en_1t <= toggle_dff_en;
+        toggle_dff_en_2t <= toggle_dff_en_1t;
+        toggle_dff_en_3t <= toggle_dff_en_2t;
+
+        if (toggle_dff_en) begin
+            toggle_dff <= !toggle_dff;
+        end
+        if (mode) begin
+            // manual mode
+            if (trigger_sr == 32'h7FFFFFFF || trigger_sr == 32'h80000000) begin
+                toggle_dff_en <= 1'b1;
+            end
+        end
+        else begin
+            // auto mode
+            toggle_dff_en <= 1'b1;
+            if (toggle_dff_en_3t == 1'b1 || toggle_dff_en_2t == 1'b1 || toggle_dff_en_1t == 1'b1 || toggle_dff_en == 1'b1) begin
+                toggle_dff_en <= 1'b0;
+            end 
+        end
+    end
+  end
+
+    // generate delay using inverters
+    assign prog_delay[0] = toggle_dff;
+    genvar i;
+    generate 
+        for (i = 0; i < 64; i = i + 1) begin
+            // add two inverters for every loop
+            assign prog_delay[i*2+1] = !prog_delay[i*2];
+            assign prog_delay[i*2+2] = !prog_delay[i*2+1];
+        end
+    endgenerate
+
+    // delay_ctrl controls how many inverters are used
+    assign delayed_toggle_dff = prog_delay[delay_ctrl*2];
+
+  always @(posedge clk or posedge rst)
+  begin
+	if(rst) begin
+                meta_dff_0        <= 1'b0;
+                meta_dff_1        <= 1'b0;
+                meta_err_detected <= 1'b0;
+    	    end 
+    else begin
+        
+        meta_dff_0 <= delayed_toggle_dff;
+        meta_dff_1 <= meta_dff_0;
+        
+        if (toggle_dff_en_3t == 1'b1) begin
+            // 3 clks after data edge, check for metastability error
+            if (toggle_dff == meta_dff_0 && meta_dff_0 == meta_dff_1) begin
+                // no error
+            end
+            else begin
+                // error detected
+                meta_err_detected <= !meta_err_detected; 
+            end
+        end
+
+    end
+  end
+
+    // assign 7 segment outputs
+    assign uo_out[0] = mode;
+    assign uo_out[1] = toggle_dff_en;
+    assign uo_out[2] = toggle_dff;
+    assign uo_out[3] = delayed_toggle_dff;
+    assign uo_out[4] = meta_dff_0;
+    assign uo_out[5] = meta_dff_1;
+    assign uo_out[6] = toggle_dff_en_3t;
+    assign uo_out[7] = meta_err_detected;
+
+endmodule
